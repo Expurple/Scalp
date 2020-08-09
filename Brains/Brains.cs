0ef80abedc4cfb0c54663c5027dfc581b7c620f2
Dmitry Alexandrov <adk230@yandex.ru>
2020-08-09 11:55:08 +0300

    added a very primitive print()

--- Brains/Brains.cs
+++ Brains/Brains.cs
@@ -16,10 +16,25 @@ namespace Scalp.Brains
 
 		public void ReactAt(string input)
 		{
-			// Language has no functions yet, so we treat it as a special case
+			// Language has no functions yet, so we treat exit() as a special case
 			if (input == "exit()")
 			{
 				ExitFlag = true;
+			}
+
+			// Language has no functions yet, so we treat print() as a special case
+			if (input.StartsWith("print(") && input.EndsWith(')'))
+			{
+				MessageFlag = true;
+				Message = input[6..^1];
+				if (! Message.EndsWith('\n'))
+				{
+					Message += '\n';
+				}
+			}
+			else
+			{
+				MessageFlag = false;
 			}
 		}
 	}
