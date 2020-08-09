9de48bbc6bbe26b9549068cf00569a9f2c20d77e
Dmitry Alexandrov <adk230@yandex.ru>
2020-08-09 14:01:39 +0300

    created primitive ScalpVariable

---
+++ CoreClasses/ScalpVariable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scalp.CoreClasses
+{
+	class ScalpVariable
+	{
+		public readonly string Name;
+		public readonly ScalpType Type;
+		public Object PrimitiveValue { get; set; }
+
+		public ScalpVariable(string name, ScalpType type)
+		{
+			Name = name;
+			Type = type;
+			PrimitiveValue = type.PrimitiveValue;
+		}
+	}
+}
