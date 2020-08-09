0e215aae8ac6ae5ed433f483e90af5cface583d7
Dmitry Alexandrov <adk230@yandex.ru>
2020-08-09 13:26:20 +0300

    Created a primitive Type

---
+++ CoreClasses/Type.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scalp.CoreClasses
+{
+	class Type
+	{
+		public readonly string TypeName;
+		public Object PrimitiveValue { get; set; }
+	}
+}
