3699783c85d8d869fb6706f7bd78586a1560842c
Dmitry Alexandrov <adk230@yandex.ru>
2020-08-17 23:27:59 +0300

    replaced a template test with the first meaningful test

---
+++ Tests/Functionality/StandardFunctions/Print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+using Scalp.Brains;
+
+namespace Tests.Functionality.StandardFunctions
+{
+	public class Print
+	{
+		[Test]
+		public void HelloWorld()
+		{
+			Brains brains = new Brains();
+			brains.ProcessLineOfCode("print(\"Hello World!\")");
+			Assert.IsTrue(brains.PrintFlag);
+			Assert.AreEqual("Hello World!", brains.PrintContents);
+		}
+	}
+}
